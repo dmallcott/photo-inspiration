f76f010a97efe818679a3808220f33526d0ad166
Daniel Mallcott <dmallcott@ustwo.com>
2017-02-27 14:22:49 +0000

    Add Pexels api service, include required models and update the gitignore

--- app/src/main/java/com/dmallcott/photoinspiration/application/ApplicationModule.java
+++ app/src/main/java/com/dmallcott/photoinspiration/application/ApplicationModule.java
@@ -3,8 +3,12 @@ package com.dmallcott.photoinspiration.application;
 import android.app.Application;
 import android.content.Context;
 import android.support.annotation.NonNull;
+import com.dmallcott.photoinspiration.data.service.PexelsService;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import dagger.Module;
 import dagger.Provides;
+import javax.inject.Singleton;
 
 @Module
 class ApplicationModule {
@@ -18,5 +22,16 @@ class ApplicationModule {
   @Provides
   Context provideContext() {
     return application;
+  }
+
+  @Provides
+  @Singleton
+  Gson provideGson() {
+    return new GsonBuilder().create();
+  }
+
+  @Provides
+  PexelsService providePexelsService(Gson gson) {
+    return PexelsService.Factory.makeService(gson);
   }
 }
