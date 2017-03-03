fa07e502d4d6612ca8d8d7cd4c43bb017b781d87
Daniel Mallcott <dmallcott@ustwo.com>
2017-03-03 10:53:00 +0000

    Update baseActivity and presenter lifecycles

--- app/src/main/java/com/dmallcott/photoinspiration/base/BaseActivity.java
+++ app/src/main/java/com/dmallcott/photoinspiration/base/BaseActivity.java
@@ -13,14 +13,39 @@ public abstract class BaseActivity<V extends BaseView> extends AppCompatActivity
 
   private BaseActivityComponent activityComponent;
 
+  /**
+   * This class will wrap the Activity lifecycle so it handles certain operations by default thus
+   * reducing boilerplate code. Overriding the #{@link AppCompatActivity#onCreate(Bundle)}
+   * method so the default behaviour is as follows:
+   *
+   *    - The dagger component for the current module get's created
+   *    - The child class injects itself to the Dagger component
+   *    - The view gets inflated
+   *    - Through ButterKnife the views are bound
+   *    - onInitialize is called to notify the child activity that the view has been initialized
+   *    - The presenter is notified the view has been attached
+   * */
   @Override
   protected void onCreate(@Nullable final Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     createComponent();
-    inject(activityComponent);
+    onInject(activityComponent);
     setContentView(getLayoutId());
     ButterKnife.bind(this);
+    onInitialize();
     getPresenter().onViewAttached(getPresenterView());
+  }
+
+  @Override
+  protected void onResume() {
+    getPresenter().onViewWillShow(getPresenterView());
+    super.onResume();
+  }
+
+  @Override
+  protected void onPause() {
+    getPresenter().onViewWillHide();
+    super.onPause();
   }
 
   @Override
@@ -29,10 +54,30 @@ public abstract class BaseActivity<V extends BaseView> extends AppCompatActivity
     super.onDestroy();
   }
 
+  protected void onInitialize() {
+
+  }
+
+  protected abstract void onInject(BaseActivityComponent component);
+
+  @LayoutRes
+  protected abstract int getLayoutId();
+
+  @NonNull
+  protected abstract BasePresenter<V> getPresenter();
+
+  @NonNull
+  protected abstract V getPresenterView();
+
   public ApplicationComponent getApplicationComponent() {
     return ((PhotoInspirationApplication) getApplication()).getApplicationComponent();
   }
 
+  /**
+   * For instrumentation testing purposes a method for overriding a BaseActivity's
+   * component has been added. This is sadly because I can't have a test runner that override the
+   * BaseActivity class. Or can I?
+   * */
   public void setComponent(@NonNull final BaseActivityComponent component) {
     this.activityComponent = component;
   }
@@ -53,15 +98,4 @@ public abstract class BaseActivity<V extends BaseView> extends AppCompatActivity
 
     return activityComponent;
   }
-
-  @LayoutRes
-  protected abstract int getLayoutId();
-
-  @NonNull
-  protected abstract BasePresenter<V> getPresenter();
-
-  @NonNull
-  protected abstract V getPresenterView();
-
-  protected abstract void inject(BaseActivityComponent component);
 }
