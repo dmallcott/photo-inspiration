99769ca93f601ca54a3d26dba727da9006add54c
Daniel Mallcott <dmallcott@ustwo.com>
2017-03-07 12:06:11 +0000

    Adding random slope values

--- app/src/main/java/com/dmallcott/photoinspiration/feature/main/views/MaskedImageView.java
+++ app/src/main/java/com/dmallcott/photoinspiration/feature/main/views/MaskedImageView.java
@@ -14,20 +14,19 @@ import com.dmallcott.photoinspiration.data.model.Photo;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Picasso.LoadedFrom;
 import com.squareup.picasso.Target;
-import java.util.Random;
 import timber.log.Timber;
-
+/**
+ * Note: Initially the loading behaviour would be displaying a gradient which is
+ * why the view is extending. This may change eventually.
+ */
 public class MaskedImageView extends MaskedGradientView {
 
   private final Paint pathPaint;
-  private Photo photo;
   private final Paint imagePaint;
 
-  private float slope;
+  private Photo photo;
   private Bitmap bitmap;
-
   private Target target;
-  private int offset;
 
   public MaskedImageView(Context context) {
     this(context, null);
@@ -38,11 +37,10 @@ public class MaskedImageView extends MaskedGradientView {
 
     imagePaint = new Paint();
     imagePaint.setColor(Color.WHITE);
-    pathPaint = new Paint();
-    pathPaint.setShadowLayer(SHADOW_HEIGHT, 0f, 0f, Color.BLACK);
 
-    // Define random slope
-    slope = 0.1f + (0.25f - 0.1f) * new Random().nextFloat();
+    pathPaint = new Paint();
+    pathPaint.setAntiAlias(true);
+    pathPaint.setShadowLayer(SHADOW_HEIGHT, 0f, 0f, Color.BLACK);
 
     target = new Target() {
       @Override
@@ -69,7 +67,6 @@ public class MaskedImageView extends MaskedGradientView {
   protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 
     final int widthSize = MeasureSpec.getSize(widthMeasureSpec);
-    final int heightSize = MeasureSpec.getSize(heightMeasureSpec);
 
     int width = 0;
     int height = 0;
@@ -79,20 +76,18 @@ public class MaskedImageView extends MaskedGradientView {
 
       width = widthSize;
       height = Math.round(photo.height() * ratio);
-      offset = - Math.round(height * slope);
       Timber.i("Measuring %s with w: %d and h: %d", photo.url(), width, height);
       Picasso.with(getContext()).load(photo.src().original()).resize(width, height).into(target);
       setMeasuredDimension(width, height);
     } else {
       super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
-
   }
 
   @Override
   protected void onDraw(Canvas canvas) {
     super.onDraw(canvas);
-    
+
     if (bitmap != null) {
       Timber.i("Drawing %s", photo.url());
       canvas.drawBitmap(processImage(bitmap), 0, 0, imagePaint);
@@ -118,9 +113,9 @@ public class MaskedImageView extends MaskedGradientView {
     if (this.photo != null) {
       Picasso.with(getContext()).cancelRequest(target);
     }
+
+    this.bitmap = null;
     this.photo = photo;
-    bitmap = null;
     invalidate();
   }
-
 }
