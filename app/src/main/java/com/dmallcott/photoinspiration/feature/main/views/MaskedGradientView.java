126aaf99363204220da657fbaeedb619fb288ea5
Daniel Mallcott <dmallcott@ustwo.com>
2017-03-07 12:06:11 +0000

    Adding random slope values

--- app/src/main/java/com/dmallcott/photoinspiration/feature/main/views/MaskedGradientView.java
+++ app/src/main/java/com/dmallcott/photoinspiration/feature/main/views/MaskedGradientView.java
@@ -15,19 +15,24 @@ import android.text.TextPaint;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.view.View;
+import java.util.Random;
 import timber.log.Timber;
 
-// IDEA: make maskedImage extend MaskedGradient so the behaviour is shared
 public class MaskedGradientView extends View {
 
-  public final static int OFFSET = 120; // Forced to do this due to the way itemDecoration work
+  public final static int OFFSET = 120; // Forced to do this due to the way itemDecoration works
+  private final static int MINIMUM_SLOPE = 40;
   protected final static int SHADOW_HEIGHT = 10;
 
   private final float ratio;
+  private final float slope;
+
   private final Paint pathPaint;
   private final Paint shadowPaint;
+
   private final TextPaint textPaint;
   private final TextPaint bodyPaint;
+
   private String header;
   private String body;
 
@@ -41,11 +46,16 @@ public class MaskedGradientView extends View {
     super(context, attrs);
 
     ratio = 1.78f; // 16:9
+    slope = new Random().nextFloat() * (OFFSET - MINIMUM_SLOPE) + MINIMUM_SLOPE;
 
     pathPaint = new Paint();
+    pathPaint.setAntiAlias(true);
     pathPaint.setColor(Color.WHITE);
+
     shadowPaint = new Paint();
+    pathPaint.setAntiAlias(true);
     shadowPaint.setShadowLayer(SHADOW_HEIGHT, 0f, 0f, Color.BLACK);
+
     textPaint = new TextPaint();
     textPaint.setAntiAlias(true);
     textPaint.setTextSize(28 * getResources().getDisplayMetrics().density);
@@ -62,7 +72,6 @@ public class MaskedGradientView extends View {
   protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 
     final int widthSize = MeasureSpec.getSize(widthMeasureSpec);
-    final int heightSize = MeasureSpec.getSize(heightMeasureSpec);
 
     pathPaint.setShader(new LinearGradient(0, 0, widthSize, 0, Color.parseColor("#8BFCFE"),
         Color.parseColor("#64A1FF"), TileMode.CLAMP));
@@ -79,17 +88,17 @@ public class MaskedGradientView extends View {
     canvas.drawPath(getPath(), shadowPaint);
     canvas.drawPath(getPath(), pathPaint);
 
-    // text
+    // Adding text and aligning to the center
     if (!TextUtils.isEmpty(header)) {
-      StaticLayout staticLayout = new StaticLayout(header, textPaint, canvas.getWidth(),
+      StaticLayout headerlayout = new StaticLayout(header, textPaint, canvas.getWidth(),
           Alignment.ALIGN_CENTER, 1.0f, 0, false);
-      canvas.translate((canvas.getWidth() / 2) - (staticLayout.getWidth() / 2),
-          (canvas.getHeight() / 2) - ((staticLayout.getHeight() / 2)));
-      staticLayout.draw(canvas);
-      StaticLayout staticLayout2 = new StaticLayout(body, bodyPaint, canvas.getWidth(),
+      canvas.translate((canvas.getWidth() / 2) - (headerlayout.getWidth() / 2),
+          (canvas.getHeight() / 2) - ((headerlayout.getHeight() / 2)));
+      headerlayout.draw(canvas);
+      StaticLayout bodyLayout = new StaticLayout(body, bodyPaint, canvas.getWidth(),
           Alignment.ALIGN_CENTER, 1.0f, 0, false);
-      canvas.translate(0, staticLayout.getHeight());
-      staticLayout2.draw(canvas);
+      canvas.translate(0, headerlayout.getHeight());
+      bodyLayout.draw(canvas);
     }
   }
 
@@ -108,7 +117,7 @@ public class MaskedGradientView extends View {
       path.moveTo(0, SHADOW_HEIGHT);
       path.lineTo(0, getHeight());
       path.lineTo(getWidth(), getHeight());
-      path.lineTo(getWidth(), OFFSET);
+      path.lineTo(getWidth(), slope);
       path.lineTo(0, SHADOW_HEIGHT);
       path.close();
     }
